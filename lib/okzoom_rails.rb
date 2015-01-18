e9357c4442585578454276868afe9a8130ca85d2
Guinsly Mondesir <guinslym @gmail.com>
2015-01-18 10:27:38 -0500

    added vendor

--- lib/okzoom_rails.rb
+++ lib/okzoom_rails.rb
@@ -1,5 +1,11 @@
 require "okzoom_rails/version"
 
 module OkzoomRails
-  # Your code goes here...
+  module Rails
+    if defined?(::Rails) and Gem::Requirement.new('>= 3.1').satisfied_by?(Gem::Version.new ::Rails.version)
+      class Rails::Engine < ::Rails::Engine
+        # this class enables the asset pipeline
+      end
+    end
+  end
 end
